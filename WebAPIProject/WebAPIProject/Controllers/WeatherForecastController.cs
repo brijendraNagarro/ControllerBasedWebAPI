199f89dd4e19fcfbd331d1919cd89fdc28e100ed
Brijendra Sharma <Brijendra.Sharma@theaccessgroup.com>
2024-08-07 12:29:58 +0530

    pushing changes

--- WebAPIProject/WebAPIProject/Controllers/WeatherForecastController.cs
+++ WebAPIProject/WebAPIProject/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebAPIProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing T", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
